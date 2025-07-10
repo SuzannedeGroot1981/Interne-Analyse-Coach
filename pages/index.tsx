2a38f63cce6e2a95641edf875556945dacdbe9ec
SuzannedeGroot1981 <Suzanne_vanasten@hotmail.com>
2025-07-10 13:02:55 +0200

    Pas icoon aan voor schrijf je analyse stap

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ export default function Home() {
               <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <div className="text-center">
                   <div className="w-12 h-12 bg-hl-lichtgroen rounded-full flex items-center justify-center mx-auto mb-4">
-                    <span className="material-symbols-sharp hl-icon-primary hl-icon-lg">description</span>
+                    <span className="material-symbols-sharp hl-icon-primary hl-icon-lg">edit</span>
                   </div>
                   <h3 className="font-gantari font-semibold text-hl-donkergroen mb-2">Schrijf je analyse</h3>
                   <h3 className="font-gantari font-semibold text-hl-donkergroen mb-2">1. Schrijf je analyse</h3>
