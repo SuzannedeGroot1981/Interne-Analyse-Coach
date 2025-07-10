6a510de59ef89cd696efc6cfdd1d7c35281251aa
SuzannedeGroot1981 <Suzanne_vanasten@hotmail.com>
2025-07-10 10:21:45 +0200

    Fix missing Link import in Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import Link from 'next/link'
 import { ReactNode } from 'react'
 
 interface LayoutProps {
